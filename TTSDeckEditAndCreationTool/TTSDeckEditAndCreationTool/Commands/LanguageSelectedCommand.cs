df3637255772cee2d0162511c09ed4cd6b62024f
Anthony <tony.raiti@gmail.com>
2022-08-04 21:04:41 -0400

    Adding the command missed in the previous commit lol

---
+++ TTSDeckEditAndCreationTool/TTSDeckEditAndCreationTool/Commands/LanguageSelectedCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTSDeckEditAndCreationTool.ViewModel;
+
+namespace TTSDeckEditAndCreationTool.Commands
+{
+    public class SelectLanguageCommand : CommandBase
+    {
+        StyleSelectionViewModel StyleSelectionVM { get; set; }
+        public SelectLanguageCommand(StyleSelectionViewModel ssvm)
+        {
+            StyleSelectionVM = ssvm;
+        }
+
+        public override void Execute(object parameter)
+        {
+            StyleSelectionVM.RefreshSelections();
+        }
+    }
+}
